f79c3f6069905b319cd950e6ca31a62a8f772946
Marcelo Alvarez <rmalvarezq@utn.edu.ec>
2021-04-27 20:10:39 -0500

    Validaciones Form Login
    solo diseño, no implementación del login

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shopflutter/src/bloc/provider.dart';
+import 'package:shopflutter/src/pages/home_page.dart';
+import 'package:shopflutter/src/pages/login_page.dart';
+import 'package:shopflutter/src/pages/producto_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        // home: HomePage(),
+        initialRoute: 'login',
+        routes: {
+          'home': (_) => HomePage(),
+          'login': (_) => LoginPage(),
+          'products': (_) => ProductoPage(),
+        },
+      ),
+    );
+  }
+}
